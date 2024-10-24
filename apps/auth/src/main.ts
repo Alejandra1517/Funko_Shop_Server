e53527daed7aafdac0e47aa902556da9f710367c
Alejandra1517 <alejandrabuitrago1517@gmail.com>
2024-10-24 10:22:28 -0500

    implementing jwt authentication

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,11 +1,33 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AuthModule } from './auth.module';
+// import { ValidationPipe } from '@nestjs/common';
+// import { Logger } from 'nestjs-pino';
+// import { ConfigService } from '@nestjs/config';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AuthModule);
+//   app.useGlobalPipes(
+//     new ValidationPipe({
+//       whitelist: true,
+//     }),
+//   );
+//   app.useLogger(app.get(Logger));
+//   const configService = app.get(ConfigService);
+//   await app.listen(configService.get('PORT'));
+// }
+// bootstrap();
+
+
 import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
 import { ConfigService } from '@nestjs/config';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
+  app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -16,21 +38,3 @@ async function bootstrap() {
   await app.listen(configService.get('PORT'));
 }
 bootstrap();
-
-
-// import { NestFactory } from '@nestjs/core';
-// import { AuthModule } from './auth.module';
-// import { ValidationPipe } from '@nestjs/common';
-// import { Logger } from 'nestjs-pino';
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AuthModule);
-//   app.useGlobalPipes(
-//     new ValidationPipe({
-//       whitelist: true,
-//     }),
-//   );
-//   app.useLogger(app.get(Logger));
-//   await app.listen(3001);
-// }
-// bootstrap();
--- apps/auth/src/users/users.controller.ts
+++ apps/auth/src/users/users.controller.ts
@@ -1,12 +1,23 @@
-// import { Controller } from '@nestjs/common';
+// import { Body, Controller, Post } from '@nestjs/common';
+// import { UsersService } from './users.service';
+// import { CreateUserDTO } from './dto/create-user.dto';
 
 // @Controller('users')
-// export class UsersController {}
+// export class UsersController {
+//   constructor(private readonly userService: UsersService) {}
 
+//   @Post()
+//   async createUser(@Body() createUserDto: CreateUserDTO) {
+//     return this.userService.create(createUserDto);
+//   }
+// }
 
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDTO } from './dto/create-user.dto';
+import { JwtAuthGuard } from '../guards/jwt-auth.guard';
+import { CurrentUser } from '../current-user.decorator';
+import { UserDocument } from './models/user.schema';
 
 @Controller('users')
 export class UsersController {
@@ -16,5 +27,10 @@ export class UsersController {
   async createUser(@Body() createUserDto: CreateUserDTO) {
     return this.userService.create(createUserDto);
   }
-}
 
+  @Get()
+  @UseGuards(JwtAuthGuard)
+  getCurrentUser(@CurrentUser() user: UserDocument) {
+    return user;
+  }
+}
